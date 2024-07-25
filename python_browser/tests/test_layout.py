9f9c838f1261857821d4e45a0cd99bdca5f6f8dc
Aurora Nou <2955566+auroranou@users.noreply.github.com>
2024-07-25 14:41:16 -0400

    update tests

--- python_browser/tests/test_layout.py
+++ python_browser/tests/test_layout.py
@@ -7,7 +7,7 @@ from python_browser.url import URL
 
 
 class TestLayout(unittest.TestCase):
-    def _init_browser(self, response_body: str):
+    def _init_browser(self, response_body: str, rtl: bool = False):
         socket.patch().start()
         url = "http://browser.engineering/example.html"
         socket.respond(
@@ -16,7 +16,7 @@ class TestLayout(unittest.TestCase):
             + b"Header1: Value1\r\n\r\n"
             + bytes(response_body, encoding="utf-8"),
         )
-        browser = Browser()
+        browser = Browser(rtl)
         browser.load(URL.create(url))
         return browser
 
@@ -24,16 +24,14 @@ class TestLayout(unittest.TestCase):
         browser = self._init_browser("abc def")
         self.assertEqual(len(browser.display_list), 2)
 
-        word1 = browser.display_list[0]
+        word1, word2 = browser.display_list[:2]
         self.assertEqual(word1.x, HSTEP)
         self.assertEqual(word1.word, "abc")
-
-        word2 = browser.display_list[1]
         self.assertGreater(word2.x, word1.x)
         self.assertEqual(word2.word, "def")
 
     def test_rtl(self):
-        browser = self._init_browser("abc def")
+        browser = self._init_browser("abc def", rtl=True)
         self.assertEqual(len(browser.display_list), 2)
 
         word1 = browser.display_list[0]
@@ -44,9 +42,7 @@ class TestLayout(unittest.TestCase):
         browser = self._init_browser('<h1 class="title">abc</h1><div>def</div>')
         self.assertEqual((len(browser.display_list)), 2)
 
-        word1 = browser.display_list[0]
-        word2 = browser.display_list[1]
-
+        word1, word2 = browser.display_list[:2]
         # First word should be centered, so x coord is greater than default
         self.assertGreater(word1.x, HSTEP)
         # Second word should be on a new line
@@ -56,8 +52,7 @@ class TestLayout(unittest.TestCase):
         browser = self._init_browser("<div>abc <sup>def</sup></div>")
         self.assertEqual(len(browser.display_list), 2)
 
-        word1 = browser.display_list[0]
-        word2 = browser.display_list[1]
+        word1, word2 = browser.display_list[:2]
         self.assertEqual(word1.y, word2.y)
 
         ascent1 = word1.font.metrics().get("ascent")
@@ -68,8 +63,7 @@ class TestLayout(unittest.TestCase):
         browser = self._init_browser("<abbr>Hello World 123</abbr>")
         self.assertEqual(len(browser.display_list), 13)
 
-        normal_char = browser.display_list[0]
-        abbr_char = browser.display_list[1]
+        normal_char, abbr_char = browser.display_list[:2]
         self.assertGreater(
             normal_char.font.metrics().get("ascent"),
             abbr_char.font.metrics().get("ascent"),
